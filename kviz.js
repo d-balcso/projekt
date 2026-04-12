d1b88c4c3dfdd360bfe378da5716209084c514d4
d-balcso <deli.balint@outlook.com>
2026-04-12 18:27:29 +0200

    jónapot tanár úr!

--- kviz.js
+++ kviz.js
@@ -14,7 +14,7 @@ function checkQuiz() {
         }
     }
 
-    // 1. feladat
+    //#region 1. kerdes
     let elso = document.querySelector('input[name="elso"]:checked');
     if (elso && elso.value === "luffy") {
         score++;
@@ -22,8 +22,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes1", false, "Monkey D. Luffy");
     }
+    //#endregion
 
-    // 2. feladat
+    //#region 2. kerdes
     let masodik = document.getElementById("kalozcsapat").value.toLowerCase();
     if (masodik.includes("straw")) {
         score++;
@@ -31,8 +32,10 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes2", false, "Straw Hat Pirates");
     }
+    //#endregion
 
-    // 3. feladat
+    //#region 3. kerdes
+    
     let harmadik = document.querySelector('input[name="q3"]:checked');
     if (harmadik && harmadik.value === "nami") {
         score++;
@@ -40,8 +43,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes3", false, "Nami");
     }
+    //#endregion
 
-    // 4. feladat
+    //#region 4. kerdes
     let negyedik = document.getElementById("kalozkiraly").value.toLowerCase();
     if (negyedik.includes("roger")) {
         score++;
@@ -49,8 +53,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes4", false, "Gol D. Roger");
     }
+    //#endregion
 
-    // 5. feladat
+    //#region 5. kerdes
     let otodik = document.querySelector('input[name="ötödik"]:checked');
     if (otodik && otodik.value === "brook") {
         score++;
@@ -58,8 +63,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes5", false, "Brook");
     }
+    //#endregion
 
-    // 6. feladat
+    //#region 6. kerdes
     let hatodik = document.querySelector('input[name="hatodik"]:checked');
     if (hatodik && hatodik.value === "sanji") {
         score++;
@@ -67,8 +73,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes6", false, "Sanji");
     }
+    //#endregion
 
-    // 7. feladat
+    //#region 7. kerdes
     let hetedik = document.getElementById("devilfruit").value.toLowerCase();
     if (hetedik.includes("nika") || hetedik.includes("gomu")) {
         score++;
@@ -76,8 +83,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes7", false, "Hito Hito no Mi, Model: Nika");
     }
+    //#endregion
 
-    // 8. feladat
+    //#region 8. kerdes
     let nyolcadik = document.querySelectorAll('input[name="nyolcadik"]:checked');
     let valaszok = Array.from(nyolcadik).map(el => el.value);
 
@@ -92,8 +100,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes8", false, "Shanks, Kaido, Blackbeard");
     }
+    //#endregion
 
-    // 9. feladat
+    //#region 9. kerdes
     let kilencedik = document.querySelector('input[name="kilencedik"]:checked');
     if (kilencedik && kilencedik.value === "worldgovernment") {
         score++;
@@ -101,8 +110,9 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes9", false, "World Government");
     }
+    //#endregion
 
-    // 10. feladat
+    //#region 10. kerdes
     let tizedik = document.querySelector('input[name="tizedik"]:checked');
     if (tizedik && tizedik.value === "imu") {
         score++;
@@ -110,23 +120,66 @@ function checkQuiz() {
     } else {
         visszajelzes("visszajelzes10", false, "Imu");
     }
+    //#endregion
+    
+    //#region 11. kerdes
+    let tizenegy = Number(slider.value);
 
-    // eredmeny kiiras
-    let eredmenyElem = document.getElementById("eredmeny");
-
-    if (!eredmenyElem) {
-        eredmenyElem = document.createElement("h2");
-        eredmenyElem.id = "eredmeny";
-        document.body.appendChild(eredmenyElem);
+    if (tizenegy >= 1120 && tizenegy <= 1180) {
+        score++;
+        visszajelzes("visszajelzes11", true, "1157");
+    } else {
+        visszajelzes("visszajelzes11", false, "1157");
     }
+    //#endregion
 
-    eredmenyElem.innerText = "Pontszám: " + score + "/10";
+    //#region 12. kerdes
+    let tizenkettedik = document.querySelector('input[name="tizenkettedik"]:checked');
+    if (tizenkettedik && tizenkettedik.value === "franky") {
+    score++;
+    visszajelzes("visszajelzes12", true, "Franky");
+    } else {
+    visszajelzes("visszajelzes12", false, "Franky");
+    }
+    //#endregion
 
-    if (score === 10) {
+    //#region 13. kerdes
+    let tizenharmadik = document.querySelectorAll('input[name="tizenharmadik"]:checked');
+    let valaszok13 = Array.from(tizenharmadik).map(el => el.value);
+
+    if (
+        valaszok13.includes("mihawk") &&
+        valaszok13.includes("crocodile") &&
+        valaszok13.includes("doflamingo") &&
+        !valaszok13.includes("luffy")
+    ) {
+        score++;
+    visszajelzes("visszajelzes13", true, "Mihawk, Crocodile, Doflamingo");
+    } else {
+    visszajelzes("visszajelzes13", false, "Mihawk, Crocodile, Doflamingo");
+    }
+    //#endregion
+
+    //#region eredmeny kiiras
+    let eredmenyElem = document.getElementById("eredmeny");
+    eredmenyElem.style.display = "block";
+    eredmenyElem.style.opacity = "1";
+    
+    eredmenyElem.innerText = "Pontszám: " + score + "/13";
+    document.getElementById("eredmeny").textContent = score;    
+
+    if (score === 13) {
         setTimeout(() => {
     window.location.href = "absoluteOP.html";
     }, 2000);
     }
+    //#endregion
 }
 
-
+// 11. kerdeshez: 
+let slider = document.getElementById("epizodok");
+let output = document.getElementById("value");
+output.textContent = slider.value;
+slider.addEventListener("input", function () {
+    output.textContent = slider.value;
+});
